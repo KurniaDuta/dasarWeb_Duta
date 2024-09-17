b31a4b7dd587d6bfc0becf562fd7a4004c409221
KurniaDuta <dutaka2945@gmail.com>
2024-09-17 08:58:01 +0700

    P4 variabel konstanta php

---
+++ Praktikum4/variabel_konstanta.php
@@ -0,0 +1,15 @@
+<?php
+$angka1 = 10;
+$angka2 = 5;
+$hasil = $angka1 + $angka2;
+echo "Hasil penjumlahan $angka1 dan $angka2 = $hasil <br>";
+
+$benar = true;
+$salah = false;
+echo "Variabel Benar = $benar, Variabel Salah = $salah <br>";
+
+define("Nama_Situs", "websiteku.com");
+define("Tahun_Berdiri", "2018");
+
+echo "Selamat datang di " . Nama_Situs . ", situs yang didirikan pada tahun " . Tahun_Berdiri . "<br>";
+?>
