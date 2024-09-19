491b62448cf24d8c9efe1b7cdb26989b60960576
KurniaDuta <dutaka2945@gmail.com>
2024-09-19 07:34:20 +0700

    for loop

--- Praktikum4/struktur_kontrol.php
+++ Praktikum4/struktur_kontrol.php
@@ -23,4 +23,17 @@ while ($jarakSaatIni < $jarakTarget) {
 }
 
 echo "Atlet tersebut memerlukan $hari hari untuk mencapai jarak $jarakTarget kilometer";
+
+echo "<br><br>";
+
+$jumlahLahan = 10;
+$tanamanPerLahan =5;
+$buahPerTaman = 10;
+$jumlahBuah = 0;
+
+for ($i=1; $i <= $jumlahLahan ; $i++) { 
+    $jumlahBuah += $tanamanPerLahan * $buahPerTaman;
+}
+
+echo "Jumlah buah yang akan dipanen adalah $jumlahBuah buah";
 ?>
