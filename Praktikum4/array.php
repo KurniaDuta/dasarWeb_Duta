c53701a6b7acb268ed9b8e413a3c5a3719a3114c
KurniaDuta <dutaka2945@gmail.com>
2024-09-19 08:29:27 +0700

    array dua dimensi

--- Praktikum4/array.php
+++ Praktikum4/array.php
@@ -9,4 +9,22 @@ foreach ($nialiSiswa as $niali) {
 }
 
 echo "daftar nilai siswa yang lulus: " . implode(", ", $nialiLulus);
+
+$daftarKaryawan = [
+    ['Alice', 7],
+    ['Bob', 3],
+    ['Charlie', 9],
+    ['David', 5],
+    ['Eva', 6]
+];
+
+$karyawanPengalamanLimaTahun = [];
+
+foreach ($daftarKaryawan as $karyawan) {
+    if ($karyawan[1] >= 5) {
+        $karyawanPengalamanLimaTahun[] = $karyawan[0];
+    }
+}
+
+echo "<br>daftar karyawan yang memiliki pengalaman lebih dari lima tahun: " . implode(", ", $karyawanPengalamanLimaTahun);
 ?>
