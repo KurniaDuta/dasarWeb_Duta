93f8fd597d37d87ded9e5ca5288be0a284a421fa
KurniaDuta <dutaka2945@gmail.com>
2024-09-24 08:35:31 +0700

    fungsi return

--- Praktikum5/PHP/fungsi.php
+++ Praktikum5/PHP/fungsi.php
@@ -1,16 +1,23 @@
 <?php
-function perkenalan($nama, $salam = "Assalamualaikum"){
-    echo $salam. ", ";
-    echo "Perkenalkan, nama saya " . $nama . "<br>";
-    echo "Senang berkenalan dengan Anda<br>";
+// function perkenalan($nama, $salam = "Assalamualaikum"){
+//     echo $salam. ", ";
+//     echo "Perkenalkan, nama saya " . $nama . "<br>";
+//     echo "Senang berkenalan dengan Anda<br>";
+// }
+
+// perkenalan("Duta", "Halo");
+
+// echo "<hr>";
+
+// $saya = "Kurnia";
+// $ucapanSalam = "Selamat pagi";
+
+// perkenalan($saya);
+
+function hitungUmur($thn_lahir, $thn_sekarang){
+    $umur = $thn_sekarang - $thn_lahir;
+    return $umur;
 }
 
-perkenalan("Duta", "Halo");
-
-echo "<hr>";
-
-$saya = "Kurnia";
-$ucapanSalam = "Selamat pagi";
-
-perkenalan($saya);
+echo "Umur saya adalah " . hitungUmur(2005, 2024) . " tahun";
 ?>
