e3d10a30e324e3b601c8035bc2d3b51915d9295d
KurniaDuta <dutaka2945@gmail.com>
2024-09-24 07:44:16 +0700

    P5 fungsi php

---
+++ Praktikum5/PHP/fungsi.php
@@ -0,0 +1,10 @@
+<?php
+function perkenalan(){
+    echo "Asalamualaikum, ";
+    echo "Perkenalkan, nama saya Duta<br>";
+    echo "Senang berkenalan dengan Anda<br>";
+}
+
+perkenalan();
+perkenalan();
+?>
