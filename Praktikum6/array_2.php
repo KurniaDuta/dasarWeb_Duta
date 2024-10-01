8016dc1eb184be4c18a2afbef34d806686428312
KurniaDuta <dutaka2945@gmail.com>
2024-10-01 08:48:06 +0700

    soal 2 add style tabel array asosiatif

---
+++ Praktikum6/array_2.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Array Asosiatif</title>
+    <style>
+        table {
+            width: 25%;
+            border-collapse: collapse;
+            margin: 10px 0;
+            font-size: 18px;
+            text-align: left;
+        }
+        th, td {
+            padding: 12px;
+            border: 1px solid #ddd;
+        }
+        tr:nth-child(even) {
+            background-color: #f2f2f2;
+        }
+        tr:hover {
+            background-color: #f1f1f1;
+        }
+        td:nth-child(1) {
+            font-weight: bold;
+        }
+    </style>
+</head>
+<body>
+    <?php
+        $dosen = [
+            'Nama' => 'Elok Nur Hamdana',
+            'Domisili' => 'Malang',
+            'Jenis Kelamin' => 'Perempuan' 
+        ];
+
+        echo "<table>";
+        foreach ($dosen as $key => $value) {
+            echo "<tr>";
+            echo "<td>$key</td>";
+            echo "<td>$value</td>";
+            echo "</tr>";
+        }
+        echo "</table>";
+    ?>
+</body>
+</html>
