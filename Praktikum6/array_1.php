6055b483e398fe1d19581939adf472c1bb249b66
KurniaDuta <dutaka2945@gmail.com>
2024-10-01 08:37:07 +0700

    P6 array indeks

---
+++ Praktikum6/array_1.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Array 1</title>
+</head>
+<body>
+    <h2>Array Terindeks</h2>
+
+    <?php
+        $listDosen=["Elok Nur Hamdana", "Unggul Pamenang", "Bagus Nugraha"];
+
+        echo $listDosen[2];
+        echo "<br>";
+        echo $listDosen[0];
+        echo "<br>";
+        echo $listDosen[1];
+    ?>
+</body>
+</html>
