c5a1cc34f5d7e72a63b6bc93056368ca8c5277bf
KurniaDuta <dutaka2945@gmail.com>
2024-10-01 09:23:37 +0700

    fungsi parameter

--- Praktikum6/fungsi.php
+++ Praktikum6/fungsi.php
@@ -1,9 +1,13 @@
 <?php
-function perkenalan(){
-    echo "Asalamualaikum, ";
-    echo "Perkenalkan, nama saya Duta<br>";
+function perkenalan($nama, $salam){
+    echo $salam. ", ";
+    echo "Perkenalkan, nama saya " . $nama . "<br>";
     echo "Senang berkenalan dengan Anda<br>";
 }
-perkenalan();
-perkenalan();
+
+perkenalan("Duta", "Halo");
+echo "<hr>";
+$saya = "Kurnia";
+$ucapanSalam = "Selamat pagi";
+perkenalan($saya, $ucapanSalam);
 ?>
