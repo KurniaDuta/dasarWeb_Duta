a4dd668a915d279b65d33f66529a6656a68c4d3b
KurniaDuta <dutaka2945@gmail.com>
2024-10-01 09:31:37 +0700

    fungsi di dalam fungsi

--- Praktikum6/fungsi.php
+++ Praktikum6/fungsi.php
@@ -1,21 +1,14 @@
 <?php
-// function perkenalan($nama, $salam = "Assalamualaikum"){
-//     echo $salam. ", ";
-//     echo "Perkenalkan, nama saya " . $nama . "<br>";
-//     echo "Senang berkenalan dengan Anda<br>";
-// }
-// perkenalan("Duta", "Halo");
-
-// echo "<hr>";
-
-// $saya = "Kurnia";
-// $ucapanSalam = "Selamat pagi";
-
-// perkenalan($saya);
 function hitungUmur($thn_lahir, $thn_sekarang){
     $umur = $thn_sekarang - $thn_lahir;
     return $umur;
 }
 
-echo "Umur saya adalah " . hitungUmur(2005, 2024) . " tahun";
+function perkenalan($nama, $salam = "Assalamualaikum"){
+    echo $salam. ", ";
+    echo "Perkenalkan, nama saya " . $nama . "<br>";
+    echo "Saya berusia " . hitungUmur(2005, 2024) . " tahun<br>";
+    echo "Senang berkenalan dengan anda<br>";
+}
+perkenalan("Duta");
 ?>
